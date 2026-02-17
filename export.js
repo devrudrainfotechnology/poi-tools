cad0b23b2f388712f7b745aa5303af0b75faff3c
devrudrainfotechnology <devrudrainfotechnology@gmail.com>
2026-02-17 15:04:59 +0530

    Update export.js

--- export.js
+++ export.js
@@ -1,40 +1,149 @@
-function exportCSV()
+
+let map;
+let panorama;
+
+let mode = "";
+
+let displayMarker = null;
+let buildingMarker = null;
+
+let poiData = [];
+
+initMap();
+
+function initMap(){
+
+let center = {lat:31.6340, lng:74.8723};
+
+map = new google.maps.Map(document.getElementById("map"),{
+zoom:13,
+center:center
+});
+
+panorama = new google.maps.StreetViewPanorama(
+document.getElementById("streetview"),
 {
+position:center,
+pov:{heading:0,pitch:0}
+});
 
-let csv="Name,Category,SubCategory,DisplayLat,DisplayLng,BuildingLat,BuildingLng\n";
+map.addListener("click", function(e){
 
-poiData.forEach(p=>
-{
+let lat = e.latLng.lat();
+let lng = e.latLng.lng();
 
-csv+=`${p.name},${p.category},${p.subcategory},${p.displayLat},${p.displayLng},${p.buildingLat},${p.buildingLng}\n`;
+panorama.setPosition({lat:lat,lng:lng});
+
+if(mode=="display"){
+
+if(displayMarker) displayMarker.setMap(null);
+
+displayMarker = new google.maps.Marker({
+position:e.latLng,
+map:map,
+icon:"http://maps.google.com/mapfiles/ms/icons/red-dot.png"
+});
+
+document.getElementById("display_lat").value=lat;
+document.getElementById("display_lng").value=lng;
+
+}
+
+if(mode=="building"){
+
+if(buildingMarker) buildingMarker.setMap(null);
+
+buildingMarker = new google.maps.Marker({
+position:e.latLng,
+map:map,
+icon:"http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+});
+
+document.getElementById("build_lat").value=lat;
+document.getElementById("build_lng").value=lng;
+
+}
+
+});
+
+document.getElementById("kmlFile").addEventListener("change",loadKML);
+
+}
+
+function setDisplayMode(){
+mode="display";
+alert("Click map to set DISPLAY location");
+}
+
+function setBuildingMode(){
+mode="building";
+alert("Click map to set BUILDING location");
+}
+
+function deleteLast(){
+
+if(buildingMarker){
+buildingMarker.setMap(null);
+buildingMarker=null;
+}
+
+else if(displayMarker){
+displayMarker.setMap(null);
+displayMarker=null;
+}
+
+}
+
+function savePOI(){
+
+let poi={
+name:poi_name.value,
+category:category.value,
+subcat:subcat.value,
+landline:landline.value,
+mobile:mobile.value,
+mobile2:mobile2.value,
+display_lat:display_lat.value,
+display_lng:display_lng.value,
+build_lat:build_lat.value,
+build_lng:build_lng.value
+};
+
+poiData.push(poi);
+
+alert("Saved");
+
+}
+
+function exportCSV(){
+
+let csv="Name,Category,SubCat,Landline,Mobile,Mobile2,DisplayLat,DisplayLng,BuildLat,BuildLng\n";
+
+poiData.forEach(p=>{
+
+csv+=`${p.name},${p.category},${p.subcat},${p.landline},${p.mobile},${p.mobile2},${p.display_lat},${p.display_lng},${p.build_lat},${p.build_lng}\n`;
 
 });
 
 let blob=new Blob([csv]);
 
 let a=document.createElement("a");
-
 a.href=URL.createObjectURL(blob);
-
-a.download="poi.csv";
-
+a.download="POI_Data.csv";
 a.click();
 
 }
 
-function exportKML()
-{
+function exportKML(){
 
 let kml='<?xml version="1.0"?><kml><Document>';
 
-poiData.forEach(p=>
-{
+poiData.forEach(p=>{
 
-kml+=`
-<Placemark>
+kml+=`<Placemark>
 <name>${p.name}</name>
 <Point>
-<coordinates>${p.displayLng},${p.displayLat}</coordinates>
+<coordinates>${p.display_lng},${p.display_lat}</coordinates>
 </Point>
 </Placemark>`;
 
@@ -45,11 +154,42 @@ kml+='</Document></kml>';
 let blob=new Blob([kml]);
 
 let a=document.createElement("a");
-
 a.href=URL.createObjectURL(blob);
-
-a.download="poi.kml";
-
+a.download="POI_Data.kml";
 a.click();
+
+}
+
+function loadKML(event){
+
+let files=event.target.files;
+
+for(let file of files){
+
+let reader=new FileReader();
+
+reader.onload=function(e){
+
+let xml=new DOMParser().parseFromString(e.target.result,"text/xml");
+
+let geojson=toGeoJSON.kml(xml);
+
+geojson.features.forEach(f=>{
+
+let coords=f.geometry.coordinates;
+
+new google.maps.Marker({
+position:{lat:coords[1],lng:coords[0]},
+map:map,
+icon:"http://maps.google.com/mapfiles/ms/icons/yellow-dot.png"
+});
+
+});
+
+};
+
+reader.readAsText(file);
+
+}
 
 }
