f0a54d8f268f04f8bf19062a409f06e2e6736741
devrudrainfotechnology <devrudrainfotechnology@gmail.com>
2026-02-18 10:42:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -45,6 +45,17 @@ document.getElementById("kmlFile").addEventListener("change", function(e)
     });
 });
 let map;
+
+function initMap()
+{
+    map = new google.maps.Map(document.getElementById("map"),
+    {
+        center: { lat: 31.6340, lng: 74.8723 },
+        zoom: 7,
+        mapTypeId: "roadmap"
+    });
+}
+
 let streetView;
 
 let displayMode=false;
@@ -226,4 +237,5 @@ updateTable();
 }
 
 window.onload=initMap;
+
 
